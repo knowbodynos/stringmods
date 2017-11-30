ba6f195ecdd1b89cb52548fa8b0ada573a8c3a28
Ross Everett Altman <altman.ro@discovery2.neu.edu>
2017-11-30 08:10:51 -0500

    Updated modules 'maxcones' to 'facets', 'facetnfinetriangs' to 'facetfinetriangs', and 'facetnallfinetriangs' to 'facetallfinetriangs'.

---
+++ modules/facetallfinetriangs/facetallfinetriangs.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+read line
+while [ "${line}" != "" ]
+do
+    nf=$(echo ${line} | jq -r ".NFORM")
+    nf2skel=$(echo ${line} | jq -r ".NFORM2SKEL")
+    #nallfinetriangs=$(echo ${nf2skel} | sed 's/{/[/g' | sed 's/}/]/g' | points2nallfinetriangs 2>/dev/null | head -c -1)
+    allfinetriangs=$(echo "{$(echo ${nf2skel} | sed 's/{/[/g' | sed 's/}/]/g' | timeout 3600 points2allfinetriangs 2>/dev/null | rev | cut -d':' -f1 | rev | sed 's/];//g' | tr '\n' ',' | head -c -1)}")
+    nallfinetriangs=$(($(echo ${allfinetriangs} | grep -o "}},{{" | wc -l)+1))
+    echo "+FACET.{\"NFORM\":\"${nf}\"}>{\"FACETNTRIANG\":${nallfinetriangs},\"FACETTRIANG\":${allfinetriangs}}"
+    echo "@"
+    sleep 0.01
+    read line
+done
---
+++ modules/facetfinetriangs/facetfinetriangs.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+read line
+while [ "${line}" != "" ]
+do
+    nf=$(echo ${line} | jq -r ".NFORM")
+    nf2skel=$(echo ${line} | jq -r ".NFORM2SKEL")
+    #nfinetriangs=$(echo ${nf2skel} | sed 's/{/[/g' | sed 's/}/]/g' | timeout 3600 points2nfinetriangs 2>/dev/null | head -c -1)
+    finetriangs=$(echo "{$(echo ${nf2skel} | sed 's/{/[/g' | sed 's/}/]/g' | timeout 3600 points2finetriangs 2>/dev/null | rev | cut -d':' -f1 | rev | sed 's/];//g' | tr '\n' ',' | head -c -1)}")
+    nfinetriangs=$(($(echo ${finetriangs} | grep -o "}},{{" | wc -l)+1))
+    echo "+FACET.{\"NFORM\":\"${nf}\"}>{\"FACETNREGTRIANG\":${nfinetriangs},\"FACETREGTRIANG\":${finetriangs}}"
+    echo "@"
+    sleep 0.01
+    read line
+done
