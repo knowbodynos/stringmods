e8cfef0957a66d9c11eb2c4e2b575466f78857b3
Ross Everett Altman <altman.ro@discovery2.neu.edu>
2017-12-23 14:26:25 -0500

    Fixed a few module issues.

--- modules/facetallfinetriangs/facetallfinetriangs.bash
+++ modules/facetallfinetriangs/facetallfinetriangs.bash
@@ -8,7 +8,7 @@ do
     #nallfinetriangs=$(echo ${nf2skel} | sed 's/{/[/g' | sed 's/}/]/g' | points2nallfinetriangs 2>/dev/null | head -c -1)
     allfinetriangs=$(echo "{$(echo ${nf2skel} | sed 's/{/[/g' | sed 's/}/]/g' | timeout 3600 points2allfinetriangs 2>/dev/null | rev | cut -d':' -f1 | rev | sed 's/];//g' | tr '\n' ',' | head -c -1)}")
     nallfinetriangs=$(($(echo ${allfinetriangs} | grep -o "}},{{" | wc -l)+1))
-    echo "+FACET.{\"NFORM\":\"${nf}\"}>{\"FACETNTRIANG\":${nallfinetriangs},\"FACETTRIANG\":${allfinetriangs}}"
+    echo "+FACET.{\"NFORM\":\"${nf}\"}>{\"FACETNTRIANG\":${nallfinetriangs},\"FACETTRIANG\":\"${allfinetriangs}\"}"
     echo "@"
     sleep 0.01
     read line
